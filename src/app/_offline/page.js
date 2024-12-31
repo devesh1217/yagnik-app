21c0aacbf599ddb15b4fc38a95363bf01b55c4ce
Devesh Mehta <devesh1217@gmail.com>
2024-12-31 11:03:44 +0530

    Offile page added

---
+++ src/app/_offline/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function OfflinePage() {
+    return (
+        <div className='w-full h-screen flex flex-col items-center justify-center pb-32'>
+            <Image src={'/offline.png'} width={300} height={300} alt='Error'></Image>
+            <h1 className='sm:text-5xl text-3xl mb-5'>You are Offline or Unkonw Network Error!</h1>
+            <div className='sm:text-3xl text-xl'>Check your internet connection and try again later</div>
+        </div>
+    )
+}
+
+export default OfflinePage
